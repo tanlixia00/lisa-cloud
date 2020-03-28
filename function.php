b84554264690fa63b1ee549a298e08549284572b
tanlixia00 <62798315+tanlixia00@users.noreply.github.com>
2020-03-28 22:11:17 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,6 +1,6 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
-$conn = new mysqli($dbhost,'root','ubaya','jerichodb');
+$conn = new mysqli($dbhost,'root','ubaya','elisaliedb');
 if ($conn->connect_error) {
     echo "Unable to connect, please try again";
     die();
